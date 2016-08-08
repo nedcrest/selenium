fd15d3479b8d0f0eb7fa66736162ac3d39b3fde8
dmytro_h <nedvancrest@gmail.com>
2016-08-08 22:51:21 +0200

    uncommented --find link by xpath class--
    uncommented

--- xpath_find.py
+++ xpath_find.py
@@ -14,26 +14,26 @@ import time
 # time.sleep(4)
 # driver.close()
 
-# # --find link by xpath class--
-# driver = webdriver.Firefox()
-# driver.get('http://google.com/')
-#
-# try:
-#     driver.find_elements_by_xpath("//a[@class='_Gs']")
-#     print "TEst passed: link text class by XPath found"
-#
-# except Exception as e:
-#     print "Exception found", format(e)
-#
-# driver.close()
-
-# --Get all Class elements XPath--
+# --find link by xpath class--
 driver = webdriver.Firefox()
 driver.get('http://google.com/')
 
-id = driver.find_elements_by_xpath('//*[@class]')
+try:
+    driver.find_elements_by_xpath("//a[@class='_Gs']")
+    print "TEst passed: link text class by XPath found"
 
-for i in id:
-    print i.get_attribute('class')
+except Exception as e:
+    print "Exception found", format(e)
 
 driver.close()
+
+# --Get all Class elements XPath--
+# driver = webdriver.Firefox()
+# driver.get('http://google.com/')
+#
+# id = driver.find_elements_by_xpath('//*[@class]')
+#
+# for i in id:
+#     print i.get_attribute('class')
+#
+# driver.close()
